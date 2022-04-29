69f3e3226ca7a870984fd94e191c17ad07c33758
Ryan O'Neill <ryanoneill416@gmail.com>
2022-04-29 20:15:35 +0000

    Begin adding core js elements

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementByID('next-btn');
+const optionButtonElements = document.getElementById('option-buttons');
+
+const roadSigns = [
+    {
+        sign: 'Sign 1',
+        options: [
+            {text: 'OptionA', correct: true},
+            {text: 'OptionB', correct: true},
+            {text: 'OptionC', correct: true},
+            {text: 'OptionD', correct: true},
+        ]
+    }
+]
