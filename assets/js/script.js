4ba65136c31c95f89df21f17c1138369ba641e95
Ryan O'Neill <ryanoneill416@gmail.com>
2022-04-29 22:03:20 +0000

    Add first three js functions as well as button eventlisteners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const startButton = document.getElementById('start-btn');
-const nextButton = document.getElementByID('next-btn');
+const nextButton = document.getElementById('next-btn');
 const optionButtonElements = document.getElementById('option-buttons');
 const signPlaceholder = document.getElementById('sign-holder');
 const questionContainer = document.getElementById('question-container');
@@ -19,18 +19,52 @@ const roadSigns = [
     }
 ]
 
-function startQuiz() {
+// learned from w3schools arrow function
+startButton.addEventListener('click', startQuiz);
+nextButton.addEventListener('click', () => {
+    currentSign++;
+    setNextSign();
+})
 
+/**
+ * The main game function which triggers the quiz to start when the useer selects the 'Start' button
+ */
+function startQuiz() {
+    startButton.classList.add('hide');
+    randomizedSigns = roadSigns.sort(() => Math.random() - .5);
+    currentSign = 0;
+    questionContainer.classList.remove('hide');
+    setNextSign();
 }
 
 function setNextSign() {
-
+    resetOptions();
+    showSign(randomizedSigns[currentSign]);
 }
 
+/**
+ * Sets current sign to answer
+ * Makes button elements for the user to make their selection
+ * Assigns correct datatype to correct answer
+ */
 function showSign(roadSign) {
-
+    signPlaceholder.innerText = roadSign.sign;
+    roadSign.options.forEach(option => {
+        const button = document.createElement('button');
+        button.innerText = option.text;
+        button.classList.add('btn');
+        if (option.correct) {
+            button.dataset.correct = option.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        optionButtonElements.appendChild(button);
+    });
 }
 
 function selectAnswer() {
-    
+
+}
+
+function resetOptions() {
+
 }
